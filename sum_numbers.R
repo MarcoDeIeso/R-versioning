a929bc4e6c34d3d6f76a9f12fc1a95fb5a8d75b6
MarcoDeieso <marco.deieso85@gmail.com>
2024-05-29 12:52:16 +0000

    Create sum_numbers_function

---
+++ sum_numbers.R
@@ -0,0 +1,10 @@
+# Define the function to sum two numbers
+sum_two_numbers <- function(a, b) {
+  # Add the two numbers
+  result <- a + b
+  # Return the result
+  return(result)
+}
+
+# Example usage
+# sum_two_numbers(3, 5)
